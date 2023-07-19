5552a8d45bed63920770c5bfaa88e487a5c70d70
Fahmi Kudo <fahmi.hidayatullah12@gmail.com>
2023-07-19 12:02:48 +0700

    initialize project

---
+++ src/main/java/com/dansmultipro/test/jobwebservices/JobWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.dansmultipro.test.jobwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dansmultipro/test/jobwebservices/JobWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dansmultipro.test.jobwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
